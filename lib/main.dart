3ec4f5c086759d24216fbbdb40ccec8c161a82ed
eveil <erick@erickveil.net>
2025-07-28 12:35:20 -0700

    Function to load the json added

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,8 @@
+import 'dart:convert';
+import 'dart:math';
+
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 
 class TarotCard {
   final String name;
@@ -69,17 +73,21 @@ class MyHomePage extends StatefulWidget {
 }
 
 class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
 
-  void _incrementCounter() {
-    setState(() {
-      // This call to setState tells the Flutter framework that something has
-      // changed in this State, which causes it to rerun the build method below
-      // so that the display can reflect the updated values. If we changed
-      // _counter without calling setState(), then the build method would not be
-      // called again, and so nothing would appear to happen.
-      _counter++;
-    });
+  TarotCard? _drawnCard;
+  bool _isLoading = false;
+  final Random _random = Random();
+
+  // Load and parse tarot card JSON
+  Future<List<dynamic>> _loadTarotCards() async {
+    final String response = await rootBundle.loadString('assets/tarot_cards.json');
+    return jsonDecode(response);
+  }
+
+  // Draw a random tarot card
+  Future<void> _drawCard() async {
+
+
   }
 
   @override
