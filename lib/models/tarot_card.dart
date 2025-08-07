aa5b6d557ffe26cd4a64acb7cc73bee3f398a2ab
eveil <erick@erickveil.net>
2025-08-07 10:39:53 -0700

    🧱 Added image path member to TarotCard model

--- lib/models/tarot_card.dart
+++ lib/models/tarot_card.dart
@@ -6,6 +6,7 @@ class TarotCard {
   final String uprightMeaning;
   final String invertedMeaning;
   final bool isUpright;
+  String? imagePath;
 
   TarotCard({
     required this.name, 
@@ -13,6 +14,7 @@ class TarotCard {
     required this.uprightMeaning,
     required this.invertedMeaning,
     required this.isUpright,
+    this.imagePath,
   });
 
   /// Factory constructor to create a TarotCard instance from JSON data.
